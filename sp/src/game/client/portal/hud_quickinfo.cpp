c0633880bb5b75e6b052e0f060c5cb7fa062d9d5
RaraFolf <80000kevin@gmail.com>
2015-07-23 15:26:40 -0400

    Swapped Portal and HL2 crosshairs
    I tried using both as a hybrid but it didn't work right.

--- sp/src/game/client/portal/hud_quickinfo.cpp
+++ sp/src/game/client/portal/hud_quickinfo.cpp
@@ -4,7 +4,7 @@
 //
 // $NoKeywords: $
 //
-//=============================================================================// 
+//=============================================================================//
 #include "cbase.h"
 #include "hud.h"
 #include "hudelement.h"
@@ -17,14 +17,6 @@
 #include "c_portal_player.h"
 #include "c_weapon_portalgun.h"
 #include "igameuifuncs.h"
-#include "../hud_crosshair.h"
-#include "VGuiMatSurface/IMatSystemSurface.h"
-
-#ifdef SIXENSE
-#include "sixense/in_sixense.h"
-#include "view.h"
-int ScreenTransform( const Vector& point, Vector& screen );
-#endif
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
@@ -59,7 +51,7 @@ public:
 	void Init( void );
 	void VidInit( void );
 	bool ShouldDraw( void );
-	virtual void OnThink();
+	//virtual void OnThink();
 	virtual void Paint();
 	
 	virtual void ApplySchemeSettings( IScheme *scheme );
@@ -69,20 +61,7 @@ private:
 	void	UpdateEventTime( void );
 	bool	EventTimeElapsed( void );
 
-	int		m_lastAmmo;
-	int		m_lastHealth;
-
-	float	m_ammoFade;
-	float	m_healthFade;
-
-	bool	m_warnAmmo;
-	bool	m_warnHealth;
-
-	bool	m_bFadedOut;
-	
-	bool	m_bDimmed;			// Whether or not we are dimmed down
-	float	m_flLastEventTime;	// Last active event (controls dimmed state)
-
+	float	m_flLastEventTime;
 	
 	float	m_fLastPlacedAlpha[2];
 	bool	m_bLastPlacedAlphaCountingUp[2];
@@ -96,11 +75,6 @@ private:
 	CHudTexture	*m_icon_lb;		// left bracket, full
 	CHudTexture	*m_icon_rbe;	// right bracket, empty
 	CHudTexture	*m_icon_lbe;	// left bracket, empty
-
-	CHudTexture	*m_icon_rbhl;		// right bracket, full
-	CHudTexture	*m_icon_lbhl;		// left bracket, full
-	CHudTexture	*m_icon_rbehl;	// right bracket, empty
-	CHudTexture	*m_icon_lbehl;	// left bracket, empty
 
 	CHudTexture	*m_icon_rbnone;	// right bracket
 	CHudTexture	*m_icon_lbnone;	// left bracket
@@ -125,46 +99,41 @@ void CHUDQuickInfo::ApplySchemeSettings( IScheme *scheme )
 	BaseClass::ApplySchemeSettings( scheme );
 
 	SetPaintBackgroundEnabled( false );
-	SetForceStereoRenderToFrameBuffer( true );
 }
-
 
 
 void CHUDQuickInfo::Init( void )
 {
-	m_ammoFade		= 0.0f;
-	m_healthFade	= 0.0f;
-
-	m_lastAmmo		= 0;
-	m_lastHealth	= 100;
-
-	m_warnAmmo		= false;
-	m_warnHealth	= false;
-
-	m_bFadedOut			= false;
-	m_bDimmed			= false;
 	m_flLastEventTime   = 0.0f;
 }
-
 
 
 void CHUDQuickInfo::VidInit( void )
 {
 	Init();
 
-	m_icon_c = gHUD.GetIcon("crosshair");
-	m_icon_rb = gHUD.GetIcon( "portal_crosshair_right_valid" );
-	m_icon_lb = gHUD.GetIcon( "portal_crosshair_left_valid" );
-	m_icon_rbe = gHUD.GetIcon( "portal_crosshair_last_placed" );
-	m_icon_lbe = gHUD.GetIcon( "portal_crosshair_last_placed" );
-	m_icon_rbn = gHUD.GetIcon( "portal_crosshair_right_invalid" );
-	m_icon_lbn = gHUD.GetIcon( "portal_crosshair_left_invalid" );
-	m_icon_rbnone = gHUD.GetIcon( "crosshair_right" );
-	m_icon_lbnone = gHUD.GetIcon( "crosshair_left" );
-	m_icon_rbhl = gHUD.GetIcon("crosshair_right_full");
-	m_icon_lbhl = gHUD.GetIcon("crosshair_left_full");
-	m_icon_rbehl = gHUD.GetIcon("crosshair_right_empty");
-	m_icon_lbehl = gHUD.GetIcon("crosshair_left_empty");
+	m_icon_c = gHUD.GetIcon( "crosshair" );
+
+	if ( IsX360() )
+	{
+		m_icon_rb = gHUD.GetIcon( "portal_crosshair_right_valid_x360" );
+		m_icon_lb = gHUD.GetIcon( "portal_crosshair_left_valid_x360" );
+		m_icon_rbe = gHUD.GetIcon( "portal_crosshair_last_placed_x360" );
+		m_icon_lbe = gHUD.GetIcon( "portal_crosshair_last_placed_x360" );
+		m_icon_rbn = gHUD.GetIcon( "portal_crosshair_right_invalid_x360" );
+		m_icon_lbn = gHUD.GetIcon( "portal_crosshair_left_invalid_x360" );
+	}
+	else
+	{
+		m_icon_rb = gHUD.GetIcon( "portal_crosshair_right_valid" );
+		m_icon_lb = gHUD.GetIcon( "portal_crosshair_left_valid" );
+		m_icon_rbe = gHUD.GetIcon( "portal_crosshair_last_placed" );
+		m_icon_lbe = gHUD.GetIcon( "portal_crosshair_last_placed" );
+		m_icon_rbn = gHUD.GetIcon( "portal_crosshair_right_invalid" );
+		m_icon_lbn = gHUD.GetIcon( "portal_crosshair_left_invalid" );
+		m_icon_rbnone = gHUD.GetIcon( "crosshair_right" );
+		m_icon_lbnone = gHUD.GetIcon( "crosshair_left" );
+	}
 }
 
 
@@ -209,70 +178,17 @@ bool CHUDQuickInfo::ShouldDraw( void )
 	if ( player == NULL )
 		return false;
 
-	if ( !crosshair.GetBool() && !IsX360() )
+	if ( !crosshair.GetBool() )
 		return false;
 
 	return ( CHudElement::ShouldDraw() && !engine->IsDrawingLoadingImage() );
 }
 
-//-----------------------------------------------------------------------------
-// Purpose: Checks if the hud element needs to fade out
-//-----------------------------------------------------------------------------
-void CHUDQuickInfo::OnThink()
-{
-	BaseClass::OnThink();
-
-	C_BasePlayer *player = C_BasePlayer::GetLocalPlayer();
-	if ( player == NULL )
-		return;
-
-	// see if we should fade in/out
-	bool bFadeOut = player->IsZoomed();
-
-	// check if the state has changed
-	if ( m_bFadedOut != bFadeOut )
-	{
-		m_bFadedOut = bFadeOut;
-
-		m_bDimmed = false;
-
-		if ( bFadeOut )
-		{
-			g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "Alpha", 0.0f, 0.0f, 0.25f, vgui::AnimationController::INTERPOLATOR_LINEAR );
-		}
-		else
-		{
-			g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "Alpha", QUICKINFO_BRIGHTNESS_FULL, 0.0f, QUICKINFO_FADE_IN_TIME, vgui::AnimationController::INTERPOLATOR_LINEAR );
-		}
-	}
-	else if ( !m_bFadedOut )
-	{
-		// If we're dormant, fade out
-		if ( EventTimeElapsed() )
-		{
-			if ( !m_bDimmed )
-			{
-				m_bDimmed = true;
-				g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "Alpha", QUICKINFO_BRIGHTNESS_DIM, 0.0f, QUICKINFO_FADE_OUT_TIME, vgui::AnimationController::INTERPOLATOR_LINEAR );
-			}
-		}
-		else if ( m_bDimmed )
-		{
-			// Fade back up, we're active
-			m_bDimmed = false;
-			g_pClientMode->GetViewportAnimationController()->RunAnimationCommand( this, "Alpha", QUICKINFO_BRIGHTNESS_FULL, 0.0f, QUICKINFO_FADE_IN_TIME, vgui::AnimationController::INTERPOLATOR_LINEAR );
-		}
-	}
-}
 
 void CHUDQuickInfo::Paint()
 {
 	C_Portal_Player *pPortalPlayer = (C_Portal_Player*)( C_BasePlayer::GetLocalPlayer() );
 	if ( pPortalPlayer == NULL )
-		return;
-
-	C_BasePlayer *player = C_BasePlayer::GetLocalPlayer();
-	if (player == NULL)
 		return;
 
 	C_BaseCombatWeapon *pWeapon = GetActiveWeapon();
@@ -296,58 +212,6 @@ void CHUDQuickInfo::Paint()
 	C_WeaponPortalgun *pPortalgun = dynamic_cast<C_WeaponPortalgun*>( pWeapon );
 
 	bool bPortalPlacability[2];
-
-	// Check our health for a warning
-	int	health	= player->GetHealth();
-	if ( health != m_lastHealth )
-	{
-		UpdateEventTime();
-		m_lastHealth = health;
-
-		if ( health <= HEALTH_WARNING_THRESHOLD )
-		{
-			if ( m_warnHealth == false )
-			{
-				m_healthFade = 255;
-				m_warnHealth = true;
-				
-				CLocalPlayerFilter filter;
-				C_BaseEntity::EmitSound( filter, SOUND_FROM_LOCAL_PLAYER, "HUDQuickInfo.LowHealth" );
-			}
-		}
-		else
-		{
-			m_warnHealth = false;
-		}
-	}
-
-	// Check our ammo for a warning
-	int	ammo = pWeapon->Clip1();
-	if ( ammo != m_lastAmmo )
-	{
-		UpdateEventTime();
-		m_lastAmmo	= ammo;
-
-		// Find how far through the current clip we are
-		float ammoPerc = (float) ammo / (float) pWeapon->GetMaxClip1();
-
-		// Warn if we're below a certain percentage of our clip's size
-		if (( pWeapon->GetMaxClip1() > 1 ) && ( ammoPerc <= ( 1.0f - CLIP_PERC_THRESHOLD )))
-		{
-			if ( m_warnAmmo == false )
-			{
-				m_ammoFade = 255;
-				m_warnAmmo = true;
-
-				CLocalPlayerFilter filter;
-				C_BaseEntity::EmitSound( filter, SOUND_FROM_LOCAL_PLAYER, "HUDQuickInfo.LowAmmo" );
-			}
-		}
-		else
-		{
-			m_warnAmmo = false;
-		}
-	}
 
 	if ( pPortalgun )
 	{
@@ -491,66 +355,6 @@ void CHUDQuickInfo::Paint()
 		//last placed portal indicator
 		m_icon_lbe->DrawSelf( xCenter - (m_icon_lbe->Width() * 1.85f), yCenter, lastPlaced2Color );
 		m_icon_rbe->DrawSelf( xCenter + (m_icon_rbe->Width() * 0.75f), yCenter, lastPlaced1Color );
-
-		float	scalar = 138.0f / 255.0f;
-		int	sinScale = (int)(fabs(sin(gpGlobals->curtime*8.0f)) * 128.0f);
-
-		// Update our health
-		if (m_healthFade > 0.0f)
-		{
-			DrawWarning(xCenter - (m_icon_lbhl ->Width() * 2), yCenter, m_icon_lbhl, m_healthFade);
-		}
-		else
-		{
-			float healthPerc = (float)health / 100.0f;
-			healthPerc = clamp(healthPerc, 0.0f, 1.0f);
-
-			Color healthColor = m_warnHealth ? gHUD.m_clrCaution : gHUD.m_clrNormal;
-
-			if (m_warnHealth)
-			{
-				healthColor[3] = 255 * sinScale;
-			}
-			else
-			{
-				healthColor[3] = 255 * scalar;
-			}
-
-			gHUD.DrawIconProgressBar(xCenter - (m_icon_lbhl->Width() * 2), yCenter, m_icon_lbhl, m_icon_lbehl, (1.0f - healthPerc), healthColor, CHud::HUDPB_VERTICAL);
-		}
-
-		// Update our ammo
-		if (m_ammoFade > 0.0f)
-		{
-			DrawWarning(xCenter + m_icon_rb->Width(), yCenter, m_icon_rbhl, m_ammoFade);
-		}
-		else
-		{
-			float ammoPerc;
-
-			if (pWeapon->GetMaxClip1() <= 0)
-			{
-				ammoPerc = 0.0f;
-			}
-			else
-			{
-				ammoPerc = 1.0f - ((float)ammo / (float)pWeapon->GetMaxClip1());
-				ammoPerc = clamp(ammoPerc, 0.0f, 1.0f);
-			}
-
-			Color ammoColor = m_warnAmmo ? gHUD.m_clrCaution : gHUD.m_clrNormal;
-
-			if (m_warnAmmo)
-			{
-				ammoColor[3] = 255 * sinScale;
-			}
-			else
-			{
-				ammoColor[3] = 255 * scalar;
-			}
-
-			gHUD.DrawIconProgressBar(xCenter + m_icon_rbhl->Width(), yCenter, m_icon_rbhl, m_icon_rbehl, ammoPerc, ammoColor, CHud::HUDPB_VERTICAL);
-		}
 	}
 }
 
